76fe66e5d3b2911ba71f8cc8a1c771d50a2b9421
ParamveerChattha <paramveersingh08.92@gmail.com>
2019-04-18 12:54:33 +0530

    fixed api cal

--- weather-report/src/components/Weather.js
+++ weather-report/src/components/Weather.js
@@ -6,9 +6,9 @@ class Weather extends React.Component{
 
 getWeather = async (e) =>{
     e.preventDefault();
-  const api_call = await  fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${APIKEY}`);
+  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${APIKEY}`);
 
-const data = await api_call.json;
+const data = await api_call.json();
 console.log(data);
 }
     render(){
