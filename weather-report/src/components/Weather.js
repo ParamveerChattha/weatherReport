b1182703ca9bdf6d57a07774549972e4e2b70512
ParamveerChattha <paramveersingh08.92@gmail.com>
2019-04-21 16:01:41 +0530

    alignment change

--- weather-report/src/components/Weather.js
+++ weather-report/src/components/Weather.js
@@ -37,6 +37,30 @@ getWeather = async (e) =>{
 //    console.log(this.state.data[0]);
 
 }
+weatherPhoto(id){
+if(id== "800" ){
+    return   <img className="image" src="https://images.pexels.com/photos/518415/pexels-photo-518415.jpeg" alt="weather image" />
+}
+if(id== "801"){
+    return <img className="image" src="https://wxmanreno.files.wordpress.com/2016/11/blue-sky-1.jpg?w=529" alt="weather image" />
+}
+if(id == "804"){
+    return  <img className="image" src="https://t3.ftcdn.net/jpg/01/55/26/12/240_F_155261294_VoNrW8hfViCJ7F3bAs66g61YC4C217aN.jpg" alt="weather image" />
+    
+}
+
+if(id == "522"){
+    return  <img className="image" src="https://static1.squarespace.com/static/5899e78b1b10e35238fba886/t/5bd0e4ca4785d33f58ec3c1c/1540416755583/shutterstock_heavy+rain.jpg" alt="weather image" />
+    
+}
+
+
+
+
+
+
+
+}
     render(){
         return(
             <div >
@@ -47,7 +71,7 @@ getWeather = async (e) =>{
             <ul>
             {this.state.data.map(item =>(
                 <li key="item.dt_txt">
-                <img className="image" src="https://wi-images.condecdn.net/image/doEYpG6Xd87/crop/2040/f/weather.jpg" alt="weather image" />
+                {this.weatherPhoto.bind(item.weather.id)}
                 <br/>
                 Date: {(item.dt_txt).substring(0,10) } <br/> Temp: {(item.main.temp -273.15).toFixed(2)} c <br/> weather: {item.weather[0].description} 
                 </li>
