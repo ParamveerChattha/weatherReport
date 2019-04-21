4296faa441c6f2eed55b16c756e38972dc05494c
ParamveerChattha <paramveersingh08.92@gmail.com>
2019-04-21 16:15:38 +0530

    fixed default value issue

--- weather-report/src/components/Weather.js
+++ weather-report/src/components/Weather.js
@@ -31,39 +31,47 @@ getWeather = async (e) =>{
     });
 
     console.log(this.state.data);
+//    console.log(this.state.data[0]);
+}
+componentWillMount = async()=> {
 
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=bangalore,IN&APPID=${APIKEY}`);
+    const dataJson = await api_call.json();
+    let temp = [];
+    for(let i = 0; i<dataJson.list.length;i+=8){
+        temp.push(dataJson.list[i]);
+       }
+    this.setState({
+        data: temp
+    });
 
-
+    console.log(this.state.data);
 //    console.log(this.state.data[0]);
 
 }
+
 weatherPhoto(id){
-if(id== "800" ){
+if(id === 800 ){
     return   <img className="image" src="https://images.pexels.com/photos/518415/pexels-photo-518415.jpeg" alt="weather image" />
 }
-if(id== "801"){
+if(id === 801){
     return <img className="image" src="https://wxmanreno.files.wordpress.com/2016/11/blue-sky-1.jpg?w=529" alt="weather image" />
 }
-if(id == "804"){
+if(id === 804){
     return  <img className="image" src="https://t3.ftcdn.net/jpg/01/55/26/12/240_F_155261294_VoNrW8hfViCJ7F3bAs66g61YC4C217aN.jpg" alt="weather image" />
     
 }
 
-if(id == "522"){
+if(id === 522){
     return  <img className="image" src="https://static1.squarespace.com/static/5899e78b1b10e35238fba886/t/5bd0e4ca4785d33f58ec3c1c/1540416755583/shutterstock_heavy+rain.jpg" alt="weather image" />
     
 }
-
-
-
-
-
-
 
 }
     render(){
         return(
             <div >
+                
        <Form getWeather={this.getWeather}/>
 <div className="card">
 
